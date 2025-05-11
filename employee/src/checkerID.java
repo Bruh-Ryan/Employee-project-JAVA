747e9b39657f75b034ff5eda39546954ebf67a3c
Bruh-Ryan <74509851+Bruh-Ryan@users.noreply.github.com>
2025-05-12 04:43:40 +0530

    Update checkerID.java

--- employee/src/checkerID.java
+++ employee/src/checkerID.java
@@ -1,15 +1,17 @@
-public class checkerID extends App{
+public class checkerID extends EmployeeSwingApp{//change if you want to use CUI for App
 
     public checkerID(GetEmp empID) {
 
 		String getID=empID.empID;
+        String getID_for_swing=empID.empID;
 
 		sendCheck(getID);
+        sendCheck(getID_for_swing);
     }
 	
 	public static GetEmp sendCheck(String get){
 		
-		for (GetEmp emp : App.dbms) {
+		for (GetEmp emp : EmployeeSwingApp.dbms) {//change if you want to use CUI for App
             if (emp.empID.equals(get)) {
                 return emp;
             }
